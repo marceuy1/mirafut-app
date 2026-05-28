9bbce4ce2258981eafa6107fb3eccf535eb1f884
marceuy1 <marceuy1@gmail.com>
2026-05-28 19:48:07 -0400

    Revert to original Supabase project

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zzqjtmhqvvyncuqhrbal.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzgwMDExNDIxLCJleHAiOjIwOTUzNzE0MjF9.Y11PlPdDCqH5hKhezz6Zm5LAhUSqJ8ladNMmvSIuuNY'
+const supabaseUrl = 'https://fsntjslzgbewxeaoqobj.supabase.co'
+const supabaseAnonKey = 'sb_publishable_vwJzmGQuEodNDlUeIwr7XA_G3uMBUqf'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
