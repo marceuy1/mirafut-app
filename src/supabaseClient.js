5561ca9b5c7cc6ac5c5ce6e1830b922442c25c73
Yaquelin Osegueda <jacky@Yaquelins-Air.attlocal.net>
2026-06-11 19:28:22 -0400

    Switch to JWT anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://fsntjslzgbewxeaoqobj.supabase.co'
-const supabaseAnonKey = 'sb_publishable_vwJzmGQuEodNDlUeIwr7XA_G3uMBUqf'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbnRqc2x6Z2Jld3hlYW9xb2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwOTg0NDAsImV4cCI6MjA5MzY3NDQ0MH0.oS4E0IEW94j6p2d4EIL3CtjlzdoZo7ICuIvIP0uXaG4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
