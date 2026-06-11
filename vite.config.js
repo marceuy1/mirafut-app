765c751cb355b243a313bc2b1649e141e2867d15
Yaquelin Osegueda <jacky@Yaquelins-Air.attlocal.net>
2026-06-11 18:46:41 -0400

    Force new bundle hash

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+      }
+    }
   }
 })
